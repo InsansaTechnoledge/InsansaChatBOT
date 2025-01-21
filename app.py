a189627e63e79fe6776fa63006915c79156caf96
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2025-01-21 09:19:45 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -72,19 +72,31 @@ def get_exam_details(exam_name):
         if exam.lower() in exam_name:
             response = {}
             if "start date" in exam_name or "starting date" in exam_name:
-                response["start_date"] = details["start_date"]
+                response["start_date"] = {
+                    "name":exam_name,
+                    "start_date": details["start_end"],
+                }
             elif "end date" in exam_name or "ending date" in exam_name:
-                response["end_date"] = details["end_date"]
+                response["end_date"] = {
+                    "name":exam_name,
+                    "end_date": details["end_date"],
+                }
             elif "link" in exam_name:
                 response["link_details"] = {
+                    "name":exam_name,
                     "url": details["url"],
                     "apply_link": details["apply"]
                 }
+
             elif "date" in exam_name:
-                response["start_date"] = details["start_date"]
-                response["end_date"] = details["end_date"]
+                response["date"] = {
+                    "name":exam_name,
+                    "start_date": details["start_date"],
+                    "end_date": details["end_date"],
+                }
             else:
                 response["exam_details"] = {
+                    "name":details["name"],
                     "url": details["url"],
                     "start_date": details["start_date"],
                     "end_date": details["end_date"],
@@ -94,10 +106,7 @@ def get_exam_details(exam_name):
             # Return the response in JSON format using jsonify
             return jsonify(response)
 
-    return "Exam not found. Please check the name and try again."
-
-# Initialize the general model
-initiate_general_model()
+    return jsonify({"response": "Exam not found. Please check the name and try again."})
 
 if __name__ == '__main__':
     app.run(debug=True)
